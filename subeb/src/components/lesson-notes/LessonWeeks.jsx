c6bc69fed0cceeb974e2ca3ecd1d79f986fd4dfb
Zeemonk1 <110388816+ZeeMonk-pixel@users.noreply.github.com>
2023-10-23 10:37:54 +0100

    about us bug fix

--- subeb/src/components/lesson-notes/LessonWeeks.jsx
+++ subeb/src/components/lesson-notes/LessonWeeks.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "./lesson-notes.css";
 import { Button, SelectInput } from '../custom-inputs/CustomInputs';
+import { DeletePopup, UpdatePopup } from '../alerts/Alerts';
+import useOverlay from '../../hooks/useOverlay';
 
 const LessonWeeks = () => {
 
@@ -28,8 +30,34 @@ const LessonWeeks = () => {
         },
     ]
 
+    const {setDisplayOverlay} = useOverlay();
+
+    const [display, setDisplay] = useState(false);
+    const [displayDelete, setDisplayDelete] = useState(false);
+
+    const clickUpdate = () =>{
+        setDisplay(!display);
+        setDisplayOverlay(true);
+    }
+    const clickRemove = () =>{
+        setDisplayDelete(!displayDelete);
+        setDisplayOverlay(true);
+    }
+
+    const cancelClick = () =>{
+        setDisplay(!display);
+        setDisplayOverlay(false);
+    }
+    const cancelDelete = () =>{
+        setDisplayDelete(!displayDelete);
+        setDisplayOverlay(false);
+    }
+
+
   return (
     <div className='lesson-weeks'>
+        <UpdatePopup display={display} title={"Update Lesson Note"} text={"Are you sure you want to update this lesson note?"} cancelBtn={cancelClick} />
+        <DeletePopup display={displayDelete} title={"Remove Lesson Note"} text={"Are you sure you want to remove this lesson note?"} cancelBtn={cancelDelete} />
         <div className="lesson-weeks_head lesson-class_head">
             <SelectInput placeholder={"Social Studies"} />        
             <SelectInput placeholder={"Primary 4"} />        
@@ -40,8 +68,8 @@ const LessonWeeks = () => {
                     <div key={idx} className="each-week">
                         <h3>{item.week}</h3>
                         <div className="each-week_btn">
-                            <Button btnText={"Update"} btnClass={"btn-small"} />
-                            <Button btnText={"Remove"} btnColor={"red"} btnClass={"btn-small-white"}  />
+                            <Button btnText={"Update"} btnClass={"btn-small"} btnClick={clickUpdate} />
+                            <Button btnText={"Remove"} btnColor={"red"} btnClass={"btn-small-white"} btnClick={clickRemove}  />
                         </div>
                     </div>
                 )
